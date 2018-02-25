949854fdaa922ba2d3c40d6f2e6d6f59b88d1ee6
marcelaf7 <marcelaf@rams.colostate.edu>
2018-02-24 19:41:08 -0700

    add comments, make code into class

--- randItem.py
+++ randItem.py
@@ -1,8 +1,14 @@
 import random
 import json
 
-datastore = json.load(open('AmazonData.JSON'))
+class RandAmazon
+	#constructor
+	#reads filename as JSON file and stores it as a dictionary in dictData and stores the size of dictionary in size
+	def __init__(self, filename):
+		self.dictData = json.load(open(filename))
+		self.size = datastore["size"]
 
-size = datastore["size"]
-
-randDict = datastore[str(random.randint(1,size))]
+	#generates a random numer within the size of the dictionary and returns item as dictionary
+	#returned dictionary contains "title", "image", and "price"
+	def generateRand():
+		return self.dictData[str(random.randint(1,size))]
